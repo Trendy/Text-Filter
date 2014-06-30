fbea0f8e01b23e5859806a387480f0e36b7100e3
Jeremiah Hoyet <jerhoyet@gmail.com>
2014-06-29 20:54:49 -0400

    Added filter symbol definition and created a new method for parsing filter list

--- filtertext.js
+++ filtertext.js
@@ -1,23 +1,34 @@
-function filterText( filters, documentText ){
-    this.filter = this.filterInput(filters);
+function filterText( filters, documentText, filterSymbol ){
+
+    this.filterSymbol = filterSymbol || "*****";
     this.documentText = documentText;
+    this.filter = this.createFilters(filters);
     this.filteredText = this.parseText();
 };
 
-filterText.prototype.filterInput = function(input){
+filterText.prototype.filterInput = function( input ){
+
     return input.replace(/[\|\\&;\$%@<>\(\)\+\[\]{}]/g, "");
 }
 
-filterText.prototype.parseText = function(){
+filterText.prototype.createFilters = function( input ){
+
+    input = this.filterInput(input);
     var pattern = /"([^"]*)"|'([^']*)'|[^\s,]+/g,
         output = [],
         m;
 
-    while ((m = pattern.exec(this.filter)) !== null)
+    while ((m = pattern.exec(input)) !== null)
     {
         output.push(m[1] || m[2] || m[0]);
     }
-    
-    return this.documentText.replace(new RegExp(output.join("|"), "g"), "XXXX");;
+
+    return output;
+};
+
+filterText.prototype.parseText = function(){
+
+    var pattern = new RegExp(this.filter.join("|"), "g");
+    return this.documentText.replace( pattern, this.filterSymbol );;
 };
 
